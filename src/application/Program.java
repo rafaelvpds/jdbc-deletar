6926913a62fe9ebd5c607296934b93df7c200e22
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-01 14:17:35 -0300

    jdbc-delete

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		
+		
+
+	}
+
+}
